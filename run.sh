196b7fca9f1b31c66a657af9697f08c6cf4a6b62
Dilik <1989deny@gmail.com>
2024-06-12 12:04:03 -0400

    updated repo

--- run.sh
+++ run.sh
@@ -1,1 +1,5 @@
+echo "Updating package list and installing dependencies..."
+apt-get update && apt-get install -y ffmpeg libsm6 libxext6
+
+echo "Starting Streamlit application..."
 python -m streamlit run app.py --server.port 8000 --server.address 0.0.0.0
